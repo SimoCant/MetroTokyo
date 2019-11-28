21d7b5070012c6b50f1e9fb73e2d8b7ea57f0978
ceciliaravera <ceciliaravera@10.156.5.221>
2019-11-28 16:31:38 +0100

    Constant stations added

--- src/Constants.java
+++ src/Constants.java
@@ -3,6 +3,41 @@ public final class Constants {
 	static final String IKEBUKURO="Ikebukuro";
 	static final String MEJIRO="Mejiro";
 	static final String YOYOGI="Yoyogi";
+	static final String SHINJUKU="Shinjuku";
+	static final String SHINOKUBO="Shin-Okubo";
+	static final String TAKADANOBABA="Takadanobaba";
+	static final String OTSUKA="Otsuka";
+	static final String SUGAMO="Sugamo";
+	static final String KOMAGOME="Komagome";
+	static final String TABATA="Tabata";
+	static final String NISHINIPPORI="Nishi-Nippori";
+	static final String NIPPORI="Nippori";
+	static final String UGUISUDANI="Uguisudani";
+	static final String UENO="Ueno";
+	static final String OKACHIMACHI="Okachimachi";
+	static final String AKIHABARA="Akihabara";
+	static final String KANDA="Kanda";
+	static final String TOKYO="Tokyo";
+	static final String YURAKUCHO="Yurakucho";
+	static final String SHIMBASHI="Shimbashi";
+	static final String HAMAMATSUCHO="Hamamatsucho";
+	static final String TAMACHI="Tamachi";
+	static final String SHINAGAWA="Shinagawa";
+	static final String OSAKI="Osaki";
+	static final String GOTANDA="Gotanda";
+	static final String MEGURO="Meguro";
+	static final String EBISU="Ebisu";
+	static final String SHIBUYA="Shibuya";
+	static final String HARAJUKU="Harajuku";	
+	static final String SENDAGAYA="Sendagaya";	
+	static final String SHINANOMACHI="Shinanomachi";	
+	static final String YOTSUYA="Yotsuya";	
+	static final String IICHIGAYA="Iichigaya";	
+	static final String IIDABASHI="Iidabashi";	
+	static final String SUIDOBASHI="Suidobashi";	
+	
+
+	
 	
 	
 }
