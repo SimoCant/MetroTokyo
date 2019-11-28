566213ea35eec69402c1e11c37d182bf87731e75
Simone <simo_cant@hotmail.it>
2019-11-28 15:59:49 +0100

    Data structure commit.

--- src/Station.java
+++ src/Station.java
@@ -1,13 +1,28 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Station {
 	
 	private String name;
+	private double latitude, longitude;
+	private HashMap<String, Double> nearStations = new HashMap<>();
+	private ArrayList<Integer> lines = new ArrayList<>();
 	
-	public Station(String name) {
+	public Station(String name, double latitude, double longitude) {
 		this.name = name;
-		System.out.print("CIAO");
+		this.latitude = latitude;
+		this.longitude = longitude;
 	}
 
 	public String getName() {
 		return name;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+	
+	public double getongitude() {
+		return longitude;
 	}
 }
