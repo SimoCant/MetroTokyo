a1dc8c80fea80a687513d0b6305b4496f785f8b3
Elia Broggio <elia.broggio@gmail.com>
2019-11-28 16:38:53 +0100

    Station Creation

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,14 @@ import java.util.HashMap;
 
 public class Main {
 	
-	public static void main(String[] args) {
-		HashMap <String, Station> stations = new HashMap<>();
-		stations.put("Ikebukuro", new Station("Ikebukuro", 139.711389, 35.730278));
-		
+	public static void main(String[] args) {	
+		 HashMap<String, Station> stations = new HashMap<String, Station>(){{
+			 put(Constants.IKEBUKURO, new Station(Constants.IKEBUKURO, 35.730278, 139.711389));
+			 
+			 
+		 }};
+		 
+		 
 		System.out.println(stations);
 	}
 }
--- src/Station.java
+++ src/Station.java
@@ -8,7 +8,7 @@ public class Station {
 	private HashMap<String, Double> nearStations = new HashMap<>();
 	private ArrayList<Integer> lines = new ArrayList<>();
 	
-	public Station(String name, double longitude, double latitude) {
+	public Station(String name, double latitude, double longitude) {
 		this.name = name;
 		this.longitude = longitude;
 		this.latitude = latitude;
