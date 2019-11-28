2d391e74c1140f33d52b1dee3fbd4d141e2b9e51
Simone <simo_cant@hotmail.it>
2019-11-28 16:23:36 +0100

    Put commit.

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,9 @@ import java.util.HashMap;
 public class Main {
 	
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		HashMap <String, Station> stations = new HashMap<>();
 		stations.put("Ikebukuro", new Station("Ikebukuro", 139.711389, 35.730278));
 		
 		System.out.println(stations);
-=======
-		// TODO Auto-generated method stub
-		System.out.println("Let's start this fuc*in' project");		
-
-		
->>>>>>> branch 'master' of https://github.com/SimoCant/MetroTokyo.git
 	}
 }
