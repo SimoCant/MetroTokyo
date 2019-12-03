356d799c4faccdf15411c79763266df93349b455
Elia Broggio <elia.broggio@gmail.com>
2019-12-03 14:35:21 +0100

    Added class MetroTokyo

---
+++ src/MetroTokyo.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+
+public class MetroTokyo {
+	public static HashMap<String, Station> estaciones = new HashMap<String, Station>(){{
+		//Put everything on the main in here
+		
+	}};			
+}
